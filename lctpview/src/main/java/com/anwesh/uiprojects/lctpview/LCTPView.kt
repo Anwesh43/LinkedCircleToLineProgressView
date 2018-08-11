09994dd8c13406469252df799fcd460cc373609c
Anwesh43 <anweshthecool0@gmail.com>
2018-08-12 02:39:12 +0530

    adding canvas extension method

--- lctpview/src/main/java/com/anwesh/uiprojects/lctpview/LCTPView.kt
+++ lctpview/src/main/java/com/anwesh/uiprojects/lctpview/LCTPView.kt
@@ -9,8 +9,31 @@ import android.view.MotionEvent
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.content.Context
+import android.graphics.Path
 
 val nodes : Int = 5
+
+fun Canvas.drawLCTPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / nodes
+    val sc1 : Float = Math.min(scale, 0.5f) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(scale - 0.5f, 0f)) * 2
+    save()
+    translate(i * gap, h/2 + h/10 * sc2)
+    val path : Path = Path()
+    for (j in 0..360) {
+        val x : Float = (gap / 2) * Math.cos(j * Math.PI / 180).toFloat()
+        val y : Float = (gap / 2) * sc1 * Math.sin(j * Math.PI/180).toFloat()
+        if (j == 0) {
+            path.moveTo(x, y)
+        } else {
+            path.lineTo(x, y)
+        }
+    }
+    drawPath(path, paint)
+    restore()
+}
 
 class LCTPView(ctx : Context) : View(ctx) {
 
