476b48e5d0af28ebd200852373b1cd9e41b8b8df
Anwesh43 <anweshthecool0@gmail.com>
2018-08-12 01:23:29 +0530

    importing libraries and defining number of nodes

--- lctpview/src/main/java/com/anwesh/uiprojects/lctpview/LCTPView.kt
+++ lctpview/src/main/java/com/anwesh/uiprojects/lctpview/LCTPView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.lctpview
 /**
  * Created by anweshmishra on 12/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 5
