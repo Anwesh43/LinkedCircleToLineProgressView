131bc440989f6ad8080e748421fb488dbea57f69
Anwesh43 <anweshthecool0@gmail.com>
2018-08-12 13:54:06 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/circletolineprogressview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/circletolineprogressview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.circletolineprogressview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.lctpview.LCTPView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LCTPView.create(this)
+        val view : LCTPView = LCTPView.create(this)
         fullScreen()
+        view.addAnimationListener({createToast("animation ${it + 1} is completed")}, {createToast("animation ${it + 1} is reset") })
+    }
+
+    private fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
